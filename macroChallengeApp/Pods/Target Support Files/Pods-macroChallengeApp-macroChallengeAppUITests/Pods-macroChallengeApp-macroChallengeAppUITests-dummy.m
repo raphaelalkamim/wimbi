53f7efa1479d16570001964de0884377599ff453
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2022-09-12 14:20:23 -0300

    install snapkit

---
+++ macroChallengeApp/Pods/Target Support Files/Pods-macroChallengeApp-macroChallengeAppUITests/Pods-macroChallengeApp-macroChallengeAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_macroChallengeApp_macroChallengeAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_macroChallengeApp_macroChallengeAppUITests
+@end
