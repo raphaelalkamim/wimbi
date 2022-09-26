887a8e8896321ef92c47546ce0db0c79773e3339
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2022-09-26 16:25:49 -0300

    emptyview implemented

---
+++ macroChallengeApp/macroChallengeApp/Flows/Current/View/CurrentEmptyState.swift
@@ -0,0 +1,72 @@
+//
+//  CurrentEmptyState.swift
+//  macroChallengeApp
+//
+//  Created by Raphael Alkamim on 26/09/22.
+//
+
+import Foundation
+import UIKit
+
+class CurrentEmptyState: UIView {
+    let designSystem: DesignSystem = DefaultDesignSystem.shared
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var title: UILabel = {
+        let label = UILabel()
+        label.stylize(with: designSystem.text.title)
+        label.text = "How about exploring and booking a new trip?"
+        return label
+    }()
+    
+    lazy var image: UIImageView = {
+        let image = UIImageView()
+        image.image = designSystem.images.flora
+        image.contentMode = .scaleToFill
+        return image
+    }()
+    
+    lazy var subtitle: UILabel = {
+        let label = UILabel()
+        label.stylize(with: designSystem.text.body)
+        label.textAlignment = .right
+        label.text = "Go to Explore and find a new trip or create your own."
+        return label
+    }()
+}
+
+extension CurrentEmptyState {
+    func setup() {
+        addSubview(title)
+        addSubview(image)
+        addSubview(subtitle)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        title.snp.makeConstraints { make in
+            make.topMargin.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(designSystem.spacing.xxLargePositive)
+        }
+        image.snp.makeConstraints { make in
+            make.top.equalTo(title.snp.bottom).offset(designSystem.spacing.xxLargePositive)
+            make.trailing.equalToSuperview()
+            make.height.equalTo(400)
+            make.width.equalTo(300)
+        }
+        subtitle.snp.makeConstraints { make in
+            make.top.equalTo(image.snp.bottom).offset(designSystem.spacing.xxLargePositive)
+            make.trailing.equalToSuperview().inset(designSystem.spacing.xxLargePositive)
+            make.width.equalToSuperview().inset(50)
+        }
+    }
+}
--- macroChallengeApp/macroChallengeApp/Flows/Current/ViewController/CurrentViewController.swift
+++ macroChallengeApp/macroChallengeApp/Flows/Current/ViewController/CurrentViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class CurrentViewController: UIViewController {
     weak var coordinator: CurrentCoordinator?
+    let currentEmptyView = CurrentEmptyState()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .backgroundPrimary
+        setup()
+    }
+}
 
+extension CurrentViewController {
+    func setup() {
+        self.view.addSubview(currentEmptyView)
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        currentEmptyView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
