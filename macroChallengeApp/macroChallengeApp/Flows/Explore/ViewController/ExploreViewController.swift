e61e9514c926a996501dd00df126c5382086f3e0
Luca Hummel <49925583+Lugaba@users.noreply.github.com>
2022-09-28 17:55:39 -0300

    Revert "Add/back search"

--- macroChallengeApp/macroChallengeApp/Flows/Explore/ViewController/ExploreViewController.swift
+++ macroChallengeApp/macroChallengeApp/Flows/Explore/ViewController/ExploreViewController.swift
@@ -10,48 +10,16 @@ import UIKit
 class ExploreViewController: UIViewController {
     weak var coordinator: ExploreCoordinator?
     let designSystem: DesignSystem = DefaultDesignSystem.shared
-    let locationSearchTable = RoadmapSearchTableViewController()
-
-    let explorerView = ExploreView()
-    var roadmaps: [RoadmapDTO] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .backgroundPrimary
-        self.setContextMenu()
-    
-        self.locationSearchTable.coordinator = coordinator
-        self.setupExplorerView()
-        explorerView.setupSearchController(locationTable: locationSearchTable)
-        explorerView.bindCollectionView(delegate: self, dataSource: self)
-        explorerView.addSearchBarNavigation(navigation: navigationItem)
-        
-        explorerView.searchBar.delegate = self
-
-        definesPresentationContext = true
-
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewRoadmap))        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        DataManager.shared.getPublicRoadmaps({ roadmaps in
-            self.roadmaps = roadmaps
-            self.explorerView.roadmapsCollectionView.reloadData()
-        })
-        //navigationController?.navigationBar.prefersLargeTitles = true
-    }
-    
+    @objc
     func addNewRoadmap() {
         coordinator?.createNewRoadmap()
     }
     
-    @objc func previewRoadmap() {
-        coordinator?.previewRoadmap()
-    }
-    
-}
-
-extension ExploreViewController: UISearchBarDelegate {
-    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
-        print("filtro")
-    }
 }
