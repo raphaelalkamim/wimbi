77d48f032ff02e94c2580db47c4d1726e5ebeb49
Carolina Ortega <70045652+cahhortega@users.noreply.github.com>
2022-12-05 12:36:09 -0300

    Update CurrentEmptyState.swift

--- macroChallengeApp/macroChallengeApp/Flows/Current/View/CurrentEmptyState.swift
+++ macroChallengeApp/macroChallengeApp/Flows/Current/View/CurrentEmptyState.swift
@@ -39,6 +39,7 @@ class CurrentEmptyState: UIView {
         let label = UILabel()
         label.stylize(with: designSystem.text.caption)
         label.textAlignment = .right
+        label.textColor = designSystem.palette.textPrimary
         label.text = "Go to the Explore screen and create a new one. That way, you'll be able to track how much time is left for your much-desired trip and when that time arrives, here you'll find a shortcut to it.".localized()
         return label
     }()
