70f165164be5f6b22e37c4dcb0ed8ea7e46ffae1
Carolina Ortega <cahortega@outlook.com>
2023-03-10 00:51:01 -0300

    Add dynamic fonts in DesignSystem

--- macroChallengeApp/macroChallengeApp/DesignSystem/Protocols/DesignText.swift
+++ macroChallengeApp/macroChallengeApp/DesignSystem/Protocols/DesignText.swift
@@ -7,12 +7,18 @@
 import UIKit
 
 struct DesignText {
+    let xxxLargeTitle: TextStyle = XxxLargeTitleTextStyle()
+    let xxLargeTitle: TextStyle = XxLargeTitleTextStyle()
+    let xLargeTitle: TextStyle = XLargeTitleTextStyle()
     let largeTitle: TextStyle = LargeTitleTextStyle()
     let mediumTitle: TextStyle = MediumTitleTextStyle()
     let title: TextStyle = TitleTextStyle()
-    let cellTitle: TextStyle = CellTitleTextStyle()
-    let infoTitle: TextStyle = InfoTitleTextStyle()
+    let smallTitle: TextStyle = SmallTitleTextStyle()
+    let headline: TextStyle = HeadlineTextStyle()
     let body: TextStyle = BodyTextStyle()
+    let subheadline: TextStyle = SubheadlineTextStyle()
+    let footnote: TextStyle = FootnoteTextStyle()
     let caption: TextStyle = CaptionTextStyle()
-    let smallCaption: TextStyle = SmallCaption()
+    let smallCaption: TextStyle = SmallCaptionTextStyle()
+    let xSmallCaption: TextStyle = XSmallCaptionTextStyle()
 }
