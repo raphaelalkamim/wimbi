9bb97b735839089094d6ed931ab4f8b77563fec3
bia.duque <biaduque7@hotmail.com>
2024-01-11 17:13:31 -0300

    fix: corrigir repositorio day e activiy

--- macroChallengeApp/macroChallengeApp/CoreData/Class/ActivityRepositoryClass.swift
+++ macroChallengeApp/macroChallengeApp/CoreData/Class/ActivityRepositoryClass.swift
@@ -12,112 +12,26 @@ import CoreData
 class ActivityRepository {
     static let shared: ActivityRepository = ActivityRepository()
     
-    private lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "macroChallengeApp")
-        container.loadPersistentStores { _, error in
-            if let erro = error {
-                preconditionFailure(erro.localizedDescription)
-            }
-            
-        }
-        return container
-    }()
-    
-    var context = RoadmapRepository.shared.context
-    
-    func saveContext() {
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Problema de contexto: \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
-    func createActivity(day: DayLocal, activity: Activity, isNew: Bool) -> ActivityLocal {
-        guard let newActivity = NSEntityDescription.insertNewObject(forEntityName: "ActivityLocal", into: context) as? ActivityLocal else { preconditionFailure() }
-        newActivity.id = Int32(activity.id)
-        self.setActivityData(fromActivity: activity, toActivity: newActivity)
-        
+    func createActivity(day: Day, activity: Activity, isNew: Bool)  {
         if isNew {
             DataManager.shared.postActivity(activity: activity, dayId: Int(day.id), activityCore: newActivity)
         }
-        
-        day.addToActivity(newActivity)
-        self.saveContext()
-        return newActivity
-    }
-    func copyActivity(day: DayLocal, activity: ActivityLocal) {
-        guard let newActivity = NSEntityDescription.insertNewObject(forEntityName: "ActivityLocal", into: context) as? ActivityLocal else { preconditionFailure() }
-        
-        newActivity.id = activity.id
-        newActivity.name = activity.name
-        newActivity.category = activity.category
-        newActivity.location = activity.location
-        newActivity.hour = activity.hour
-        newActivity.budget = activity.budget
-        newActivity.currencyType = activity.currencyType
-        newActivity.tips = activity.tips
-        newActivity.address = activity.address
-        newActivity.link = activity.link
-        
-        day.addToActivity(newActivity)
-        
-        self.saveContext()
-    }
-    func updateActivity(day: DayLocal, oldActivity: ActivityLocal, activity: Activity) {
-        oldActivity.id = Int32(activity.id)
-        self.setActivityData(fromActivity: activity, toActivity: oldActivity)
-        if activity.category.isEmpty { oldActivity.category = oldActivity.category }
-        
-        DataManager.shared.putActivity(activity: activity, dayId: Int(day.id))
-        
-        self.saveContext()
     }
     
-    func setActivityData(fromActivity: Activity, toActivity: ActivityLocal) {
-        toActivity.name = fromActivity.name
-        toActivity.category = fromActivity.category
-        toActivity.location = fromActivity.location
-        toActivity.hour = fromActivity.hour
-        toActivity.budget = fromActivity.budget
-        toActivity.currencyType = fromActivity.currency
-        toActivity.tips = fromActivity.tips
-        toActivity.address = fromActivity.address
-        toActivity.link = fromActivity.link
-    }
-    func updateActivityDay(roadmap: RoadmapLocal, oldDay: DayLocal, activityLocal: ActivityLocal, newActivity: Activity) {
-        if activityLocal.day?.date != newActivity.day?.date {
-            // chama os dias do roadmap
-            guard let allDays = roadmap.day?.allObjects as? [DayLocal] else { return }
-            for day in allDays where day.date == newActivity.day?.date {
-                    // adiciona a atividade no dia encontrado no coreData
-                _ = self.createActivity(day: day, activity: newActivity, isNew: true)
-            }
-            // deleta a atividade do dia antigo
-            do {
-                try self.deleteActivity(activity: activityLocal)
-            } catch {
-                print("Erro ao deletar")
-            }
-        }
-    }
-    func getActivity() -> [ActivityLocal] {
-        let fetchRequest = NSFetchRequest<ActivityLocal>(entityName: "ActivityLocal")
-        do {
-            return try self.persistentContainer.viewContext.fetch(fetchRequest)
-        } catch {
-            print(error)
-        }
-        return []
+    func copyActivity(day: Day, activity: Activity) {
+       
     }
     
-    func deleteActivity(activity: ActivityLocal) throws {
+    func updateActivity(day: Day, oldActivity: Activity, activity: Activity) {
+        var newActivity = activity
+        newActivity.id = oldActivity.id
+        
+        if newActivity.category.isEmpty { newActivity.category = oldActivity.category }
+        
+        DataManager.shared.putActivity(activity: newActivity, dayId: Int(day.id))
+    }
+    
+    func deleteActivity(activity: Activity) throws {
         DataManager.shared.deleteObjectBack(objectID: Int(activity.id), urlPrefix: "activities")
-        
-        context.delete(activity)
-        saveContext()
     }
 }
--- macroChallengeApp/macroChallengeApp/CoreData/Class/DayRepositoryClass.swift
+++ macroChallengeApp/macroChallengeApp/CoreData/Class/DayRepositoryClass.swift
@@ -12,56 +12,16 @@ import CoreData
 class DayRepository: NSManagedObject {
     static let shared: DayRepository = DayRepository()
     
-    private lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "macroChallengeApp")
-        container.loadPersistentStores { _, error in
-            if let erro = error {
-                preconditionFailure(erro.localizedDescription)
-            }
-            
-        }
-        return container
-    }()
-    
-    var context = RoadmapRepository.shared.context
-
-    func saveContext() {
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Problema de contexto: \(nserror), \(nserror.userInfo)")
-            }
-        }
+    func createDay(roadmap: Roadmap, day: Day) {
+        
     }
     
-    func createDay(roadmap: RoadmapLocal, day: Day) -> DayLocal {
-        guard let newDay = NSEntityDescription.insertNewObject(forEntityName: "DayLocal", into: context) as? DayLocal else { preconditionFailure() }
-        
-        newDay.id = Int32(day.id)
-        newDay.date = day.date
-        newDay.isSelected = day.isSelected ?? false
-        
-        roadmap.addToDay(newDay)
-        
-        self.saveContext()
-        return newDay
-    }
-    
-    func getDay() -> [DayLocal] {
-        let fetchRequest = NSFetchRequest<DayLocal>(entityName: "DayLocal")
-        do {
-            return try self.persistentContainer.viewContext.fetch(fetchRequest)
-        } catch {
-            print(error)
-        }
+    func getDay() -> [Day] {
         return []
     }
     
-    func deleteDay(day: DayLocal) throws {
-        context.delete(day)
-        try saveContext()
+    func deleteDay(day: Day) throws {
+
     }
     
 }
